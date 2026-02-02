c4f138b8b913477c9a1051d6123b6b45aa5ef4a4
serkan-erol <serkanerol.m@gmail.com>
2026-02-02 22:43:42 +0300

    Feat: Restart and Restart to go to BIOS options added Add: Restart and Restart to BIOS functionality(requires admin priviliges) Update: Refactored the code to divide functionality into functions; now that it has more functionality, complexity and re-used code

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 ShutdownTimer
 
-        - A simple scheduled shutdown to help you with shutting down your PC 
-    after a download or something similar. 
+        - A simple shutdown scheduler to help with shutting down or restarting
+    your PC after a timer. You can schedule it to shutdown/restart after a download, 
+    an update or something similar.
         
         - It uses Windows' own shutdown.exe
     It is not really different from creating a shortcut of said shutdown.exe 
     and entering command line arguments in its properties tab. Only plus side, 
-    with this one, you do not have to go to properties, and you can enter 
-    the amount of time in minutes instead of seconds. Saving precious seconds!
+    with this one, you do not have to go to properties everytime you want to change
+    the time setting, and you can enter the amount of time in minutes instead of seconds. 
+    Saving precious seconds!
 
-        - Now it also has an option to cancel a scheduled shutdown! It does
-    the job of 2 shortcuts in one simple program.
+        - Now it also has an option to cancel a scheduled shutdown/restart! 
 
-        - If you have already scheduled a shutdown via this program or 
-    shutdown.exe, and you run this program again to schedule another shutdown,
-    this action will override* the previous scheduled shutdown.
-    *Note: It will cancel the old one and schedule a new one.
+        - Now, restart and restart to BIOS options have been added, and they support
+    timers, as well. Restart to BIOS requires admin privileges to work. 
+    So, if you want to go straight to BIOS after a restart, run the program as administrator.
+    In total, it does the job of 4 shortcuts in one simple program.
+
+        - If you have already scheduled a shutdown/restart via this program or 
+    shutdown.exe, and you run this program again to schedule another shutdown or
+    restart, this action will override* the previous scheduled shutdown.
+    *Note: It will cancel the old one and schedule a new one.        
 
         - It is self-contained, meaning you do not need anything else for it to work. 
     It is 64-bit Windows only. You can find the exe file at: 
--- ShutdownTimer.cs
+++ ShutdownTimer.cs
@@ -5,91 +5,103 @@
 
 using System.Diagnostics;
 
+// String array for the main options
+string[] mainOptions = new string[5]
+{ 
+    "1 - Schedule shutdown",
+    "2 - Cancel shutdown",
+    "3 - Restart computer",
+    "4 - Restart and go to BIOS (Requires 'Run as administrator')",
+    "5 - Exit (ESC works too)\n"
+};
+
+// ConsoleKey array for valid main menu options
+ConsoleKey[] mainKeys = new ConsoleKey[11]
+{
+    ConsoleKey.D1,
+    ConsoleKey.NumPad1, // Digit 1 and NumPad 1 for option 1
+    ConsoleKey.D2,
+    ConsoleKey.NumPad2,
+    ConsoleKey.D3,
+    ConsoleKey.NumPad3,
+    ConsoleKey.D4,
+    ConsoleKey.NumPad4,
+    ConsoleKey.D5,
+    ConsoleKey.NumPad5, // Digit 5 and NumPad 5 for exit option
+    ConsoleKey.Escape  // Escape key to exit, as well
+};
+
+// Display the main menu / Ask the user for an operation
 Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine("\nChoose the operation you want to do:");
 Console.ResetColor();
-Console.WriteLine("1 - Schedule shutdown");
-Console.WriteLine("2 - Cancel shutdown");
-Console.WriteLine("3 - Restart computer");
-Console.WriteLine("4 - Exit\n");
-
-// Read user input (a single key press) without displaying it on the console
-ConsoleKeyInfo input = Console.ReadKey(true);
-
-// Get the pressed key (Enum of ConsoleKey)
-var option = input.Key; // D1 or NumPad1 for option 1, D2 or NumPad2 for option 2
-
-// Validate input: loop until the user provides a valid option (1 or 2)
-while (option != ConsoleKey.D1 &&       // Digit 1
-       option != ConsoleKey.NumPad1 &&  // NumPad 1
-       option != ConsoleKey.D2 &&       // Digit 2
-       option != ConsoleKey.NumPad2 &&  // NumPad 2
-       option != ConsoleKey.D3 &&       // Digit 3
-       option != ConsoleKey.NumPad3 &&  // NumPad 3
-       option != ConsoleKey.D4 &&       // Digit 4
-       option != ConsoleKey.NumPad4)    // NumPad 4
+foreach (string mainOption in mainOptions)
 {
-    // Clear the console before re-displaying the menu
-    Console.Clear();
-    Console.ForegroundColor = ConsoleColor.Yellow;
-
-    Console.WriteLine("\nPlease choose a valid option.");
-    Console.ResetColor();
-    Console.WriteLine("1 - Schedule shutdown");
-    Console.WriteLine("2 - Cancel shutdown");
-    Console.WriteLine("3 - Restart computer");
-    Console.WriteLine("4 - Exit\n");
-
-    input = Console.ReadKey(true);
-    option = input.Key;
+    Console.WriteLine(mainOption);
 }
 
+// Read user input (a single key press) without displaying it on the console and get the Key info (Enum of ConsoleKey)
+// Also, validate input for the main menu using the InputValidation function
+ConsoleKey option = await InputValidation(mainKeys, mainOptions);
+
 // Clear the console before next steps
-Console.Clear();
+//aaa Console.Clear();
 
 // Schedule shutdown option
-if (option == ConsoleKey.D1 ||
-    option == ConsoleKey.NumPad1)
+switch (option)
+{
+    // Call the ScheduleShutdown function
+    case ConsoleKey.D1:
+    case ConsoleKey.NumPad1:
+        await ScheduleShutdown();
+        break;
+
+    // Call the CancelShutdown function
+    case ConsoleKey.D2:
+    case ConsoleKey.NumPad2:
+        await CancelShutdown();
+        break;
+
+    // Call the RestartComputer function. Call it with biosFlag = false
+    case ConsoleKey.D3:
+    case ConsoleKey.NumPad3:
+        await RestartComputer(false);
+        break;
+
+    // Call the RestartComputer function with the 'go to BIOS' option. Call it with biosFlag = true
+    case ConsoleKey.D4:
+    case ConsoleKey.NumPad4:
+        await RestartComputer(true);
+        break;
+
+    // Exit option - do nothing, will exit the application
+    case ConsoleKey.D5:
+    case ConsoleKey.NumPad5:
+    case ConsoleKey.Escape:
+    default:
+        Console.WriteLine("Exiting application...");
+        break;
+}
+
+//aaa await Task.Delay(300); // Small delay to ensure the user kinda sees the final message
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
+//aaa Console.Clear();
+
+// ------------------------------------------------------------------------------------------------------------- //
+// ------------------------------------------------- FUNCTIONS ------------------------------------------------- //
+// ------------------------------------------------------------------------------------------------------------- //
+
+// Function to schedule a shutdown
+async Task ScheduleShutdown()
 {
     try
     {
-        Console.WriteLine("Checking if there is already a shutdown scheduled...");
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("If there is, it will be cancelled!");
+        // First, call the CancelShutdown function to cancel any existing scheduled shutdown or restart
+        await CancelShutdown();
 
-        Console.ForegroundColor = ConsoleColor.Green;
-        // Check and cancel any existing shutdown timer
-        Process cancelExistingShutdownProcess = new Process();
-        cancelExistingShutdownProcess.StartInfo.FileName = "shutdown.exe";
-        cancelExistingShutdownProcess.StartInfo.Arguments = "/a";
-        cancelExistingShutdownProcess.Start();
-        await Task.Delay(300); // Small delay to ensure the process has time to complete
-        Console.WriteLine("Cancellation completed.");
-        Console.ResetColor();
-
-        Console.WriteLine("\nHow many minutes do you need before shutdown?");
-        Console.Write("Minutes: ");
-
-        // Read, parse, and validate the input
-        // If the input can't be parsed to an integer shutdownTimeMins will be -1
-        int shutdownTimeMins = int.TryParse(Console.ReadLine(), out shutdownTimeMins) ? shutdownTimeMins : -1;
-
-        /* If the input can't be parsed to an integer (user entered a non-number value, like a String) 
-         * or the user enters a negative value
-         * the user is prompted to enter a valid number.
-         */
-        while (shutdownTimeMins < 1)
-        {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\nPlease enter a valid positive number.");
-            Console.ResetColor();
-            Console.Write("Minutes: ");
-            shutdownTimeMins = int.TryParse(Console.ReadLine(), out shutdownTimeMins) ? shutdownTimeMins : -1;
-        }
-
-        Console.Clear();
-
+        // Read user input and validate it
+        int shutdownTimeMins = await TimerInputValidation();
         // Convert minutes to seconds because the shutdown.exe takes the parameter as time in seconds 
         int shutdownTimeSecs = shutdownTimeMins * 60;
 
@@ -98,8 +110,10 @@ if (option == ConsoleKey.D1 ||
         shutdownProcess.StartInfo.FileName = "shutdown.exe";
         shutdownProcess.StartInfo.Arguments = $"/s /t {shutdownTimeSecs}";
 
+        // Start the shutdown process
         shutdownProcess.Start();
 
+        // Inform the user that the shutdown timer has been set
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("\nShutdown timer has been set.");
 
@@ -107,33 +121,36 @@ if (option == ConsoleKey.D1 ||
         switch (shutdownTimeMins)
         {
             case < 60:
-        Console.WriteLine($"Computer will shutdown in {shutdownTimeMins} minutes");
+                Console.WriteLine($"Computer will shutdown in {shutdownTimeMins} minutes");
                 break;
             default:
-        Console.WriteLine($"Computer will shutdown in {shutdownTimeMins / 60} hours " +
-                          $"and {shutdownTimeMins % 60} minutes");
+                Console.WriteLine($"Computer will shutdown in {shutdownTimeMins / 60} hours " +
+                                  $"and {shutdownTimeMins % 60} minutes");
                 break;
         }
-        Console.ResetColor();
     }
     catch (Exception ex)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"Error: {ex.Message}");
-        Console.ResetColor();
     }
     finally
     {
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
+        Console.ResetColor();
     }
 }
-// Cancel shutdown option
-else if (option == ConsoleKey.D2 ||
-         option == ConsoleKey.NumPad2) // option == 2
+
+// ------------------------------------------------------------------------------------------------------------- //
+
+// Function to cancel a scheduled shutdown
+async Task CancelShutdown()
 {
     try
     {
+        // Cancel any existing shutdown first
+        Console.WriteLine("\nChecking if there is already a shutdown scheduled...");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("If there is, it will be cancelled!");
         Console.WriteLine("Cancelling shutdown...");
 
         // Create and configure a shutdown process
@@ -146,53 +163,138 @@ else if (option == ConsoleKey.D2 ||
         Console.ForegroundColor = ConsoleColor.Green;
         await Task.Delay(300); // Small delay to ensure the process has time to complete
         Console.WriteLine("Cancellation completed.");
-        Console.ResetColor();
     }
     catch (Exception ex)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"Error: {ex.Message}");
-        Console.ResetColor();
     }
     finally
     {
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
+        Console.ResetColor();
     }
 }
-// Restart option
-else if (option == ConsoleKey.D3 ||
-         option == ConsoleKey.NumPad3) // option == 3
+
+// ------------------------------------------------------------------------------------------------------------- //
+
+// Function to restart the computer
+async Task RestartComputer(bool biosFlag)
 {
     try
     {
-        Console.WriteLine("Restarting computer...");
+        // First, call the CancelShutdown function to cancel any existing scheduled shutdown or restart
+        await CancelShutdown();
+
+        // Read user input (a single key press) without displaying it on the console
+        int restartMins = await TimerInputValidation();
+        int restartSecs = restartMins * 60;
+
+        Console.WriteLine("\nRestarting computer...");
         // Create and configure a restart process
         Process restartProcess = new Process();
         restartProcess.StartInfo.FileName = "shutdown.exe";
-        restartProcess.StartInfo.Arguments = "/r /f";
+
+        // Build the arguments string based on biosFlag and immediateFlag
+        string restartArguments = "/r" + (biosFlag ? " /fw" : " /f") + $" /t {restartSecs}";
+
+        restartProcess.StartInfo.Arguments = restartArguments;
+
+        Console.ForegroundColor = ConsoleColor.Red;
         restartProcess.Start();
+        await Task.Delay(300); // Small delay to ensure the process has time to complete
 
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Restart command issued.");
+        Console.WriteLine("Restart scheduling completed.");
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+    finally
+    {
         Console.ResetColor();
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------- //
+
+// Function to get valid input from the user with specified range and options
+async Task<ConsoleKey> InputValidation(ConsoleKey[] validKeys, string[] inputOptions)
+{
+    try
+    {
+        // Read user input (a single key press) without displaying it on the console and get the Key info (Enum of ConsoleKey)
+        ConsoleKey option = Console.ReadKey(true).Key;
+
+        // Validate input: loop until the user provides a valid option
+        while (Array.IndexOf(validKeys, option) == -1)
+        {
+            // Clear the console before re-displaying the menu
+            //aaa Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nPlease choose a valid option.");
+            Console.ResetColor();
+            foreach (string inputOption in inputOptions)
+            {
+                Console.WriteLine(inputOption);
+            }
+            option = Console.ReadKey(true).Key;
+        }
+        return option;
     }
     catch (Exception ex)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"Error: {ex.Message}");
         Console.ResetColor();
+        return ConsoleKey.Escape; // Return Escape in case of error
     }
     finally
     {
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
+        Console.ResetColor();
     }
 }
-else // option == 4
-{
-    Console.WriteLine("Exiting application...");
-    await Task.Delay(300); // Small delay before exiting
-}
 
-Console.Clear();
+// Function get valid timer input from the user
+async Task<int> TimerInputValidation()
+{
+    try
+    {
+        Console.WriteLine("\nHow many minutes do you need before shutdown/restart?");
+        Console.WriteLine("Enter 0 (zero) for immediate shutdown/restart.");
+        Console.Write("Minutes: ");
+
+        // Read, parse, and validate the input
+        // If the input can't be parsed to an integer shutdownTimeMins will be -1
+        int shutdownTimeMins = int.TryParse(Console.ReadLine(), out shutdownTimeMins) ? shutdownTimeMins : -1;
+
+        /* If the input can't be parsed to an integer (user entered a non-number value, like a String) 
+         * or the user enters a negative value
+         * the user is prompted to enter a valid number.
+         */
+        while (shutdownTimeMins < 0)
+        {
+            //aaa Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nPlease enter a valid positive number or 0 (zero).");
+            Console.WriteLine("Entering 0 will shutdown/restart immediately.");
+            Console.ResetColor();
+            Console.Write("Minutes: ");
+            shutdownTimeMins = int.TryParse(Console.ReadLine(), out shutdownTimeMins) ? shutdownTimeMins : -1;
+        }
+
+        return shutdownTimeMins;
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error: {ex.Message}");
+        Console.ResetColor();
+        return -1; // Return -1 in case of error
+    }
+    finally
+    {
+        Console.ResetColor();
+    }
+}
